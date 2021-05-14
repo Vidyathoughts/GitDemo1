c9ad1b53df8111374cf2522b65029790f0921eef
vidya <vidyatwins@gmail.com>
2021-05-14 14:37:09 +0530

    new commit1

---
+++ src/test/java/qaclickacademy/ExtentReportsDemo.java
@@ -0,0 +1,48 @@
+package qaclickacademy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportsDemo {
+	ExtentReports extent;
+	
+		@BeforeTest
+		public void config()
+		{
+			//ExtentReports, ExtentSparkReporter
+			String path = System.getProperty("user.dir")+"\\reports\\index.html";
+			ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+			
+			reporter.config().setReportName("Web Automation Results");
+			reporter.config().setDocumentTitle("Test Results");
+			
+			extent = new ExtentReports();
+			
+			extent.attachReporter(reporter);
+			extent.setSystemInfo("Tester", "Vidya");
+			
+			
+			
+		}
+		
+		@Test
+		public void initialDemo()
+		{
+			
+			ExtentTest test = extent.createTest("Initial Demo");
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\91988\\Desktop\\Selenium_setup\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			
+			driver.get("https://rahulshettyacademy.com");
+			System.out.println(driver.getTitle());
+			driver.close();
+			test.fail("result does not match");
+			extent.flush();
+		}
+
+}
